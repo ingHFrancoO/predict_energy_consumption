9f1162c6b861fb7e100e729cb997003e83b789be
ingHFrancoO <inghfrancoo@gmail.com>
2024-05-01 15:54:49 -0500

    Finish data exploration and save transform dataset in parquet and csv init analysis process

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Prediction of energy consumption, based on data set of energy consumption of 5 customers.',
+    author='ingHFrancoO',
+    license='MIT',
+)
